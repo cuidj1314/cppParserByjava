332458877584423b878fb4bbbd0be6f1cae51e55
cuidj <dejun.cui@dhc.com.cn>
2022-07-07 16:56:19 +0800

    format modify

--- src/main/java/com/work/listener/ListenerRewrite.java
+++ src/main/java/com/work/listener/ListenerRewrite.java
@@ -4,10 +4,10 @@ import com.parser.CPP14Parser;
 import com.parser.CPP14ParserBaseListener;
 
 public class ListenerRewrite extends CPP14ParserBaseListener {
-        @Override
-        public void exitRefqualifier(CPP14Parser.RefqualifierContext ctx) {
-            final String a = ctx.getChild(0).getText();
-            final String b = ctx.getChild(1).getText();
-            System.out.println(a + " " + b);
+    @Override
+    public void exitRefqualifier(CPP14Parser.RefqualifierContext ctx) {
+        final String a = ctx.getChild(0).getText();
+        final String b = ctx.getChild(1).getText();
+        System.out.println(a + " " + b);
     }
 }
